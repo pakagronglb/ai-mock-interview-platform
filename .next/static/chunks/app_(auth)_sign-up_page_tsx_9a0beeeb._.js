3f67d67e8f7b8634f3990fe83aa5dd515770edef
pakagronglb <pakagronglebel@gmail.com>
2025-03-22 16:52:49 +0700

    Add env config

---
+++ .next/static/chunks/app_(auth)_sign-up_page_tsx_9a0beeeb._.js
@@ -0,0 +1,17 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_(auth)_sign-up_page_tsx_9a0beeeb._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_(auth)_sign-up_page_tsx_9a0beeeb._.js",
+  "chunks": [
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_0217979b._.js",
+    "static/chunks/node_modules_next_433f1a0e._.js",
+    "static/chunks/node_modules_zod_lib_index_mjs_ee760afb._.js",
+    "static/chunks/node_modules_@firebase_auth_dist_esm2017_27f74358._.js",
+    "static/chunks/node_modules_@firebase_firestore_dist_index_esm2017_c2fcaa2e.js",
+    "static/chunks/node_modules_41ef64f9._.js",
+    "static/chunks/_8f6ebf3c._.js"
+  ],
+  "source": "dynamic"
+});
